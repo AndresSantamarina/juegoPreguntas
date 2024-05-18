76d6fd9c64aea27e9d88045b4f6b75418cd94b70
Andres Santamarina <andresesantamarina@gmail.com>
2024-05-18 02:13:33 -0300

    agregar preguntas terminado

--- src/components/pages/Agregar.jsx
+++ src/components/pages/Agregar.jsx
@@ -1,48 +1,178 @@
-import { Container } from "react-bootstrap";
+import { Container, FormGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import CardPregunta from "../CardPregunta";
+import { useParams } from "react-router-dom";
+import { crearPreguntaAPI } from "../../helpers/queries";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 const Agregar = () => {
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const { id } = useParams();
+
+  const crearPregunta = async (pregunta) => {
+    const respuesta = await crearPreguntaAPI(pregunta);
+    if (respuesta.status === 201) {
+      Swal.fire({
+        title: "Pregunta creada",
+        text: `La pregunta fue creada correctamente`,
+        icon: "success",
+      });
+      reset();
+    } else {
+      Swal.fire({
+        title: "Ocurrió un error",
+        text: `La pregunta no pudo ser creada`,
+        icon: "error",
+      });
+    }
+  };
+
   return (
     <Container className="mainSection">
       <h1 className="text-center my-5">Configurar el juego</h1>
       <section>
         <h3 className="my-3">Agregar preguntas</h3>
         <div className="d-flex justify-content-center my-5">
-          <Form className="w-50">
-          <Form.Select aria-label="Default select example">
-            <option>Seleccione el nivel al que quiere agregar la pregunta</option>
-            <option value="1">Uno</option>
-            <option value="2">Dos</option>
-            <option value="3">Tres</option>
-            <option value="4">Cuatro</option>
-            <option value="5">Cinco</option>
-            <option value="6">Seis</option>
-            <option value="7">Siete</option>
-            <option value="8">Ocho</option>
-            <option value="9">Nueve</option>
-            <option value="10">Diez</option>
-          </Form.Select>
+          <Form className="w-50" onSubmit={handleSubmit(crearPregunta)}>
+            <FormGroup className="mb-3" controlId="formNivel">
+              <Form.Label>Nivel</Form.Label>
+              <Form.Select
+                aria-label="Selector de niveles"
+                {...register("nivel", {
+                  required: "Seleccione un nivel",
+                })}
+              >
+                <option value="">
+                  Seleccione el nivel al que quiere agregar la pregunta
+                </option>
+                <option value={1}>Uno</option>
+                <option value={2}>Dos</option>
+                <option value={3}>Tres</option>
+                <option value={4}>Cuatro</option>
+                <option value={5}>Cinco</option>
+                <option value={6}>Seis</option>
+                <option value={7}>Siete</option>
+                <option value={8}>Ocho</option>
+                <option value={9}>Nueve</option>
+                <option value={10}>Diez</option>
+              </Form.Select>
+              <Form.Text className="text-danger">
+                {errors.nivel?.message}
+              </Form.Text>
+            </FormGroup>
+
             <Form.Group className="mb-3" controlId="formPregunta">
               <Form.Label>Pregunta</Form.Label>
-              <Form.Control type="text" placeholder="Ingrese una pregunta" />
+              <Form.Control
+                type="text"
+                placeholder="Ingrese una pregunta"
+                {...register("pregunta", {
+                  required: "La pregunta es obligatoria",
+                  minLength: {
+                    value: 5,
+                    message: "La pregunta debe tener como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La pregunta debe tener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.pregunta?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formOpcionUno">
               <Form.Label>Opción 1</Form.Label>
-              <Form.Control type="text" placeholder="Opción uno" />
+              <Form.Control
+                type="text"
+                placeholder="Opción uno"
+                {...register("opcionUno", {
+                  required: "La opción es obligatoria",
+                  minLength: {
+                    value: 5,
+                    message: "La opción debe tener como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La opción debe tener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.opcionUno?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formOpcionDos">
               <Form.Label>Opción 2</Form.Label>
-              <Form.Control type="text" placeholder="Opción dos" />
+              <Form.Control
+                type="text"
+                placeholder="Opción dos"
+                {...register("opcionDos", {
+                  required: "La opción es obligatoria",
+                  minLength: {
+                    value: 5,
+                    message: "La opción debe tener como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La opción debe tener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.opcionDos?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formOpcionTres">
               <Form.Label>Opción 3</Form.Label>
-              <Form.Control type="text" placeholder="Opción tres" />
+              <Form.Control
+                type="text"
+                placeholder="Opción tres"
+                {...register("opcionTres", {
+                  required: "La opción es obligatoria",
+                  minLength: {
+                    value: 5,
+                    message: "La opción debe tener como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La opción debe tener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.opcionTres?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formOpcionCorrecta">
               <Form.Label>Opción Correcta</Form.Label>
-              <Form.Control type="text" placeholder="Opción correcta" />
+              <Form.Control
+                type="text"
+                placeholder="Opción correcta"
+                {...register("opcionCorrecta", {
+                  required: "La opción es obligatoria",
+                  minLength: {
+                    value: 5,
+                    message: "La opción debe tener como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "La opción debe tener como máximo 50 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.opcionCorrecta?.message}
+              </Form.Text>
             </Form.Group>
             <div className="text-center">
               <Button variant="primary" type="submit">
