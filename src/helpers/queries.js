5ebc91ac2c5f19866642219be150175eab91c446
Andres Santamarina <andresesantamarina@gmail.com>
2024-05-18 01:42:31 -0300

    agregue las queries y la conexion a la bd

---
+++ src/helpers/queries.js
@@ -0,0 +1,62 @@
+const URL_Preguntas = import.meta.env.VITE_API_PREGUNTAS;
+const URL_Pregunta = import.meta.env.VITE_API_PREGUNTA_INDIVIDUAL;
+
+export const leerPreguntasAPI = async () =>{
+    try {
+        const respuesta = await fetch(URL_Preguntas)
+        const listaPreguntas = await respuesta.json()
+        return listaPreguntas
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const obtenerPreguntaAPI = async (id) =>{
+    try {
+        const respuesta = await fetch(URL_Pregunta + '/' + id)
+        return respuesta
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const crearPreguntaAPI = async (preguntaNueva)=>{
+    try {
+        const respuesta = await fetch(URL_Preguntas,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(preguntaNueva)
+        })
+        return respuesta
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const editarPreguntaAPI = async (preguntaModificada, id) =>{
+    try {
+        const respuesta = await fetch(`${URL_Pregunta}/${id}`,{
+            method:"PUT",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(preguntaModificada)
+        })
+        return respuesta
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const eliminarPreguntaAPI = async (id) =>{
+    try {
+        const respuesta = await fetch (`${URL_Pregunta}/${id}`,{
+            method:"DELETE"
+        })
+        return respuesta
+    } catch (error) {
+        console.error(error)
+    }
+}
